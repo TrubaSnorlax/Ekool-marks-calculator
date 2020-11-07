54b200f358b9a251f0eb1e4393f0933077526a71
TrubaSnorlax <55359966+TrubaSnorlax@users.noreply.github.com>
2020-11-07 23:08:59 +0000

    First commit!
    First commit (1.1.1)

---
+++ EkoolMarksCalculator/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace EkoolMarksCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lesson = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.average = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.until5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "ekool.html";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.lesson,
+            this.average,
+            this.grade,
+            this.until5});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(749, 396);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // lesson
+            // 
+            this.lesson.HeaderText = "lesson";
+            this.lesson.Name = "lesson";
+            this.lesson.ReadOnly = true;
+            this.lesson.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.lesson.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.lesson.Width = 167;
+            // 
+            // average
+            // 
+            this.average.HeaderText = "average";
+            this.average.Name = "average";
+            this.average.ReadOnly = true;
+            this.average.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.average.Width = 167;
+            // 
+            // grade
+            // 
+            this.grade.HeaderText = "grade";
+            this.grade.Name = "grade";
+            this.grade.ReadOnly = true;
+            this.grade.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.grade.Width = 167;
+            // 
+            // until5
+            // 
+            this.until5.HeaderText = "5 Marks until mark 5";
+            this.until5.Name = "until5";
+            this.until5.ReadOnly = true;
+            this.until5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.until5.Width = 210;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(789, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(789, 489);
+            this.Name = "Form1";
+            this.Text = "Ekool marks counter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lesson;
+        private System.Windows.Forms.DataGridViewTextBoxColumn average;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn until5;
+    }
+}
+
---
+++ EkoolMarksCalculator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace EkoolMarksCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+
+        private void button1_Click(object sender, EventArgs e) //Button click event
+        {
+            openFileDialog1.ShowDialog(); //Opens file dialogue
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) //File opened event
+        {
+            dataGridView1.Rows.Clear();
+
+            doc.Load(openFileDialog1.FileName); //Loads HTML document from file
+
+
+            var rows = doc.DocumentNode.SelectNodes("//table[@class='fulltable gradesheet velvet-table gray-headers kov']/tbody/tr"); //Selects rows from table
+            Dictionary<string, List<int>> grades = new Dictionary<string, List<int>>(); //Creates dictionary with scheme <subject(key), List<grades>(value)>
+
+
+
+            for (int i = 0; i < rows.Count; i++) //I'm stupid and too lazy to fix that
+            {
+                string name = rows[i].SelectNodes("//div[@class='name']")[i].InnerText.Trim(); //Gets name of a subject
+
+
+                var gradeElements = rows[i].ChildNodes[7].ChildNodes;
+                
+
+                grades.Add(name, new List<int>()); //Adds subject to dictionary
+
+                 for (int x = 0; x < gradeElements.Count; x++)//adds every mark to list
+                 {
+
+                     if (gradeElements[x].ChildNodes[1].InnerText.Trim().Trim('*') == "1" || gradeElements[x].ChildNodes[1].InnerText.Trim().Trim('*') == "2" || gradeElements[x].ChildNodes[1].InnerText.Trim().Trim('*') == "3" || gradeElements[x].ChildNodes[1].InnerText.Trim().Trim('*') == "4" || gradeElements[x].ChildNodes[1].InnerText.Trim().Trim('*') == "5")//Checks for a mark
+                     {
+                         int grade = Int32.Parse(gradeElements[x].ChildNodes[1].InnerText.Trim().Trim('*')); //creates a Int variable of it
+                         grades[name].Add(grade); //adds it to the list
+                     }
+
+                 }
+                 if (grades[name].Count == 0) //If there is no marks, delete it
+                 {
+                     grades.Remove(name);
+                 }
+             }
+
+             foreach (var name in grades.Keys)
+             {
+                 dataGridView1.Rows.Add(name, grades[name].Average(), Math.Round(grades[name].Average()), countMarksUntilFive(grades[name])); //Adds data to dataGridView (name of a subject, average mark, rounded average, five marks you need to get mark 5 in total)
+             }
+
+            }
+
+             string countMarksUntilFive(List<int> grades) //method to count a count of five marks to get mark five in total
+             {
+                 int marksUntilFive = 0;
+                 string marksUntilFiveResultString = "";
+
+                 double average = grades.Average(); //gets average mark
+
+                 if (Math.Round(average) == 5) //If you already have 5 it returns 0
+                 {
+                     return "0";
+                 }
+                 else
+                 {
+                     while (Math.Round(average) != 5) //does code while mark isn't 5
+                     {
+                         grades.Add(5); //add 5 to grades list
+                         marksUntilFive++; //adds one to marks until five count
+                         average = grades.Average(); //counts average again
+                     }
+                     marksUntilFiveResultString = marksUntilFive.ToString(); //makes count string
+                 }
+                 return marksUntilFiveResultString; //returns result
+             }
+
+
+
+
+        }
+    }
+
